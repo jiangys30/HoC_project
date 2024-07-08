b702c24a9b317edc35b8c17b734370c3c8cea82a
jiangys30 <118042246+jiangys30@users.noreply.github.com>
2024-07-09 00:07:43 +0800

    File related to Fig 5a

---
+++ Figures/Fig5, FigS11/fig_5a.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(vegan)
+
+# import feature table
+ftt <- read.csv("reads_count_for_distboxplot.csv", header = TRUE, row.names=1, sep = ",")
+
+# delete redundant columns
+ftt <- t((ftt[rowSums(ftt[, -1]) != 0, ]))
+
+# calculate distance matrix
+require(vegan)
+dm <- vegdist(ftt, method="bray")
+dm <- as.matrix(dm)
+
+# import metadata as variable
+meta <- read.csv("meta.csv", header = TRUE, row.names=1, sep = ",")
+time <- as.factor(meta[, 7])
+host <- as.factor(meta[, 8])
+DistBoxplot(dm, group = time, group_name="AMPM_", dm_name="BC", IndividualID = host)
